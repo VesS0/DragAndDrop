a2e05d1f0363d90bc070472360cc44a410abde1b
Vladimir Vesic <vlvesic@microsoft.com>
2017-05-15 18:04:41 +0200

    DragAndDrop init

---
+++ DropMe.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PhotoViewerCSharp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class DropMe : Page
+    {
+        public DropMe()
+        {
+            this.InitializeComponent();
+        }
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+        }
+
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.Bitmap))
+            {
+                RandomAccessStreamReference thumbnail = await e.DataView.GetBitmapAsync();
+                IRandomAccessStreamWithContentType stream = await thumbnail.OpenReadAsync();
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.SetSource(stream);
+                Image.Source = bitmapImage;
+            }
+        }
+
+        private void Grid_DragOverCustomized(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            e.DragUIOverride.Caption = "Drop here to view"; // Sets custom UI text
+            e.DragUIOverride.SetContentFromBitmapImage(null); // Sets a custom glyph
+            e.DragUIOverride.IsCaptionVisible = true; // Sets if the caption is visible
+            e.DragUIOverride.IsContentVisible = true; // Sets if the dragged content is visible
+            e.DragUIOverride.IsGlyphVisible = true; // Sets if the glyph is visibile
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PhotoViewerCSharp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private StorageFile imageFile;
+        private Image image;
+        private IRandomAccessStream stream;
+        private BitmapImage bitmapimage;
+        private GridView grid;
+        private IRandomAccessStreamWithContentType draggedStream;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            grid = new GridView();
+            grid.Margin = new Thickness(5, 5, 5, 5);
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CoreApplicationView newView = CoreApplication.CreateNewView();
+            int newViewId = 0;
+            await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Frame frame = new Frame();
+                frame.Navigate(typeof(MainPage), null); //DropMe
+                Window.Current.Content = frame;
+                // You have to activate the window in order to show it later.
+                Window.Current.Activate();
+
+                newViewId = ApplicationView.GetForCurrentView().Id;
+            });
+            bool viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
+        }
+
+        private async Task<StorageFile> PickImageFileAsync()
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            openPicker.FileTypeFilter.Add(".jpg");
+            openPicker.FileTypeFilter.Add(".png");
+            openPicker.FileTypeFilter.Add(".jpeg");
+
+            return await openPicker.PickSingleFileAsync();
+        }
+
+        private async void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            this.SelectImage.IsEnabled = false;
+
+            this.imageFile = await PickImageFileAsync();
+            await ShowImage();
+
+            this.SelectImage.IsEnabled = true;
+        }
+
+        private async Task ShowImage()
+        {
+            bitmapimage = new BitmapImage();
+            stream = await imageFile.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            await bitmapimage.SetSourceAsync(stream);
+
+            this.image = new Image();
+            this.image.Source = bitmapimage;
+            double maxDim = 700;
+            if (image.Width > image.Height)
+            {
+                image.Width = maxDim;
+            }
+            else
+            {
+                image.Height = maxDim;
+            }
+
+            //image.PointerPressed += new Windows.UI.Xaml.Input.PointerEventHandler(SaveSelectedImage_PointerPressed);
+
+            
+            grid.Items.Add(image);
+            Results.Items.Add(grid);
+        }
+        private void Grid_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+        }
+        private async void Grid_Drop(object sender, DragEventArgs e)
+        {
+            if (e.DataView.Contains(StandardDataFormats.StorageItems))
+            {
+                var items = await e.DataView.GetStorageItemsAsync();
+                imageFile = items[0] as StorageFile;
+                if (items.Count > 0)
+                {
+                    var storageFile = items[0] as StorageFile;
+                    var bitmapImage = new BitmapImage();
+                    bitmapImage.SetSource(await storageFile.OpenAsync(FileAccessMode.Read));
+                    // Set the image on the main page to the dropped image
+                    Image.Source = bitmapImage;
+                }
+            }
+            else
+            if (e.DataView.Contains(StandardDataFormats.Bitmap))
+            {
+                RandomAccessStreamReference thumbnail = await e.DataView.GetBitmapAsync();
+                draggedStream = await thumbnail.OpenReadAsync();
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.SetSource(draggedStream);
+                Image.Source = bitmapImage;
+            }
+        }
+        private async void Image_DragStarting(UIElement sender, DragStartingEventArgs args)
+        {
+            args.Data.RequestedOperation = DataPackageOperation.Copy;
+            //args.Data.SetStorageItems(files);
+            if (imageFile!=null) { 
+                var str = await imageFile.OpenAsync(Windows.Storage.FileAccessMode.Read);
+                args.Data.SetBitmap(RandomAccessStreamReference.CreateFromStream(str));
+            }
+            else
+            {
+                args.Data.SetBitmap(RandomAccessStreamReference.CreateFromStream(draggedStream));
+            }
+        }
+
+        private void Grid_DragOverCustomized(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+            e.DragUIOverride.Caption = "Custom text here"; // Sets custom UI text
+            e.DragUIOverride.SetContentFromBitmapImage(null); // Sets a custom glyph
+            e.DragUIOverride.IsCaptionVisible = true; // Sets if the caption is visible
+            e.DragUIOverride.IsContentVisible = true; // Sets if the dragged content is visible
+            e.DragUIOverride.IsGlyphVisible = true; // Sets if the glyph is visibile
+        }
+
+        private async void SaveSelectedImage_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            Windows.UI.Input.PointerPoint ptrPt=e.GetCurrentPoint(Results);
+            Size size = new Windows.Foundation.Size(200, 200);
+
+            Image img = new Image();
+            img.Source= await GetCroppedBitmapAsync(ptrPt.Position, size, 1);
+
+            double maxDim = 700;
+            if (img.Width > img.Height)
+            {
+                img.Width = maxDim;
+            }
+            else
+            {
+                img.Height = maxDim;
+            }
+ 
+            grid.Items.Add(img);
+            //Results.Items.Add(grid);
+        }
+
+        async public Task<ImageSource> GetCroppedBitmapAsync(
+    Point startPoint, Size corpSize, double scale)
+        {
+            if (double.IsNaN(scale) || double.IsInfinity(scale))
+            {
+                scale = 1;
+            }
+
+
+            // Convert start point and size to integer. 
+            uint startPointX = (uint)Math.Floor(startPoint.X * scale);
+            uint startPointY = (uint)Math.Floor(startPoint.Y * scale);
+            uint height = (uint)Math.Floor(corpSize.Height * scale);
+            uint width = (uint)Math.Floor(corpSize.Width * scale);
+
+
+            using (IRandomAccessStream stream = await imageFile.OpenReadAsync())
+            {
+                // Create a decoder from the stream. With the decoder, we can get  
+                // the properties of the image. 
+                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+
+                // The scaledSize of original image. 
+                uint scaledWidth = (uint)Math.Floor(decoder.PixelWidth * scale);
+                uint scaledHeight = (uint)Math.Floor(decoder.PixelHeight * scale);
+
+
+
+                // Refine the start point and the size.  
+                if (startPointX + width > scaledWidth)
+                {
+                    startPointX = scaledWidth - width;
+                }
+
+
+                if (startPointY + height > scaledHeight)
+                {
+                    startPointY = scaledHeight - height;
+                }
+
+
+                // Create cropping BitmapTransform and define the bounds. 
+                BitmapTransform transform = new BitmapTransform();
+                BitmapBounds bounds = new BitmapBounds();
+                bounds.X = startPointX;
+                bounds.Y = startPointY;
+                bounds.Height = height;
+                bounds.Width = width;
+                transform.Bounds = bounds;
+
+
+                transform.ScaledWidth = scaledWidth;
+                transform.ScaledHeight = scaledHeight;
+
+                // Get the cropped pixels within the bounds of transform. 
+                PixelDataProvider pix = await decoder.GetPixelDataAsync(
+                    BitmapPixelFormat.Bgra8,
+                    BitmapAlphaMode.Straight,
+                    transform,
+                    ExifOrientationMode.IgnoreExifOrientation,
+                    ColorManagementMode.ColorManageToSRgb);
+                byte[] pixels = pix.DetachPixelData();
+
+
+                // Stream the bytes into a WriteableBitmap 
+                WriteableBitmap cropBmp = new WriteableBitmap((int)width, (int)height);
+                Stream pixStream = cropBmp.PixelBuffer.AsStream();
+                pixStream.Write(pixels, 0, (int)(width * height * 4));
+
+                return cropBmp;
+            }
+        }
+    }
+}
